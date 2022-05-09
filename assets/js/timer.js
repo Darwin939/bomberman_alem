afc7b1bc5e2a9a6416fe49c8b13e375dfe27ce8f
utebayev.mm <utebayev.mm@gmail.com>
2022-05-09 14:11:08 +0600

    timer counts seconds only now

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -4,13 +4,11 @@ var countDownDate = new Date(new Date().getTime() + time);
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("timer").innerHTML = 
-  minutes + "m " + seconds + "s ";
+  var seconds = Math.floor((distance ) / 1000);
+  document.getElementById("timer").innerHTML = "Time left: " + seconds + "s ";
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
+    document.getElementById("timer").innerHTML = "No time left!";
   }
 }, 1000);
