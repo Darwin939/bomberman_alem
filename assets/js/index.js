e995144dd4d8e5b2cf578ece3a51b395a1db8c72
utebayev.mm <utebayev.mm@gmail.com>
2022-05-23 21:13:48 +0600

    fps lock

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,12 +13,24 @@ export let game = new Game
 
 game.start()
 
+var updateId,
+    previousDelta = 0,
+    fpsLimit = 60;
 
 
 function gameLoop(){
     game.update()
     game.draw()
-    requestAnimationFrame(gameLoop)
+
+    updateId = requestAnimationFrame(gameLoop);
+
+    var delta = currentDelta - previousDelta;
+
+    if (fpsLimit && delta < 1000 / fpsLimit) {
+        return;
+    }
+    previousDelta = currentDelta;
+
 }
 
 gameLoop()
